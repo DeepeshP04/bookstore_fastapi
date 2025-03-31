5155c67e590d9774355a5b4f285743eddae67447
DeepeshP04 <pratapdeepesh4@gmail.com>
2025-03-31 23:32:34 +0530

    Update services and main.py to include get book by id

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 import services, models, schemas
 from database import get_db, engine
 from sqlalchemy.orm import Session
@@ -8,6 +8,13 @@ app = FastAPI()
 @app.get("/books/", response_model=list[schemas.Book])
 def get_all_books(db: Session = Depends(get_db)):
     return services.get_books(db)
+
+@app.get("/books/{book_id}", response_model=schemas.Book)
+def get_book_by_id(book_id: int, db: Session = Depends(get_db)):
+    book_queryset = services.get_book_by_id(db, book_id)
+    if book_queryset:
+        return book_queryset
+    raise HTTPException(status_code=404, detail="Book not found")
 
 @app.post("/books/", response_model=schemas.Book)
 def create_new_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
--- services.py
+++ services.py
@@ -11,3 +11,6 @@ def create_book(db: Session, data: BookCreate):
 
 def get_books(db: Session):
     return db.query(Book).all()
+
+def get_book_by_id(db: Session, book_id: int):
+    return db.query(Book).filter(Book.id == book_id).first()
