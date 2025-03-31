5e6974f03f8d2582e2d3617e1b5ac34ed1be0f5f
DeepeshP04 <pratapdeepesh4@gmail.com>
2025-03-31 23:01:36 +0530

    Create a book model (table)

--- database.py
+++ database.py
@@ -12,4 +12,23 @@ POSTGRES_USER = os.getenv('POSTGRES_USER')
 POSTGRES_PASS = os.getenv('POSTGRES_PASS')
 
 # initialize the database URL
-DATABASE_URL = "postgresql://{POSTGRES_USER}:{POSTGRES_PASS}@localhost:5432/bookstore"
+DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASS}@localhost:5432/bookstore"
+
+# create engine for managing the database connection
+engine = create_engine(DATABASE_URL)
+
+# create a session local class for managing sessions
+# and a base class for all the models or tables
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+# create the database tables
+def create_table():
+    Base.metadata.create_all(bind=engine)
